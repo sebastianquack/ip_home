749676ceb44107fa81861e5b492bf08513fa651c
Sebastian Quack <sebastianquack@gmail.com>
2012-12-17 00:26:47 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpHome::Application.config.secret_token = '46461cef9f3fdfbf2aea9098ee1a48674ba52d9e91b6d4451909fcc82d1e0dc41164d093588b1331310dc902be9746a788a3d6c913b123baeb2fe88f1ac9ec74'
---
+++ db/migrate/20121216232135_create_pages.rb
@@ -0,0 +1,15 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :title
+      t.string :slug
+      t.text :content
+
+      t.timestamps
+    end
+    
+    
+    add_index :pages, :slug, unique: true
+
+  end
+end
