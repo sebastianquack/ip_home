18f2b481c38bc5176ec6af37106e41cc7689b833
Sebastian Quack <sebastianquack@gmail.com>
2014-03-19 03:42:48 +0100

    helpers for page links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,35 @@ module ApplicationHelper
         "<strong>Image with id #{$1} not found.</strong>"
       end      
     }
+    
+    text = text.gsub(/<a page:(\S+)>(\S+)<\/a>/) {
+      begin
+        page = Page.find("#{$1}")
+        link_to "#{$2}", public_page_url(page) 
+      rescue
+        "<strong>Page with id #{$1} not found.</strong>"
+      end      
+    }
+
+    text = text.gsub(/<a project:(\S+)>(\S+)<\/a>/) {
+      begin
+        project = Project.find("#{$1}")
+        link_to "#{$2}", public_project_url(project) 
+      rescue
+        "<strong>Project with id #{$1} not found.</strong>"
+      end      
+    }
+
+    text = text.gsub(/<a event:(\S+)>(\S+)<\/a>/) {
+      begin
+        event = Event.find("#{$1}")
+        link_to "#{$2}", public_event_url(event) 
+      rescue
+        "<strong>Event with id #{$1} not found.</strong>"
+      end      
+    }
+
+
 
     text.html_safe
   end
